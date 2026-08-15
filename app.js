c40132f6d7dc95ae2cc4782bee501aca91e44224
Maryam Naim <maryam29122004@gmail.com>
2026-08-15 13:33:59 +0530

    Add homepage route

--- app.js
+++ app.js
@@ -113,7 +113,9 @@ app.use((req, res, next) => {
 
 
 // ==================== ROUTES ====================
-
+app.get("/", (req, res) => {
+    res.redirect("/listings");
+});
 app.use("/listings", listingRouter);
 app.use("/ai", aiRouter);
 app.use("/listings/:id/reviews", reviewRouter);
